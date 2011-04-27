3e6388b919535bb02dbd9bacf8c68f87fcb5e125
Mark Probst <mark.probst@gmail.com>
2011-04-27 22:22:43 +0200

    Don't wait for other threads in the delayed free list.

--- delayed-free.c
+++ delayed-free.c
@@ -82,15 +82,29 @@ get_entry (int index)
 void
 mono_delayed_free_push (MonoDelayedFreeItem item)
 {
-	int index = InterlockedIncrement (&num_used_entries) - 1;
-	Entry *entry = get_entry (index);
+	int index, num_used;
+	Entry *entry;
 
-	while (InterlockedCompareExchange (&entry->state, STATE_BUSY, STATE_FREE) != STATE_FREE)
-		;
+	do {
+		index = InterlockedIncrement (&num_used_entries) - 1;
+		entry = get_entry (index);
+	} while (InterlockedCompareExchange (&entry->state, STATE_BUSY, STATE_FREE) != STATE_FREE);
+
 	mono_memory_write_barrier ();
+
 	entry->item = item;
+
 	mono_memory_write_barrier ();
+
 	entry->state = STATE_USED;
+
+	mono_memory_write_barrier ();
+
+	do {
+		num_used = num_used_entries;
+		if (num_used > index)
+			break;
+	} while (InterlockedCompareExchange (&num_used_entries, index + 1, num_used) != num_used);
 }
 
 gboolean
@@ -100,17 +114,14 @@ mono_delayed_free_pop (MonoDelayedFreeItem *item)
 	Entry *entry;
 
 	do {
-		index = num_used_entries;
-		if (index == 0)
-			return FALSE;
-	} while (InterlockedCompareExchange (&num_used_entries, index - 1, index) != index);
+		do {
+			index = num_used_entries;
+			if (index == 0)
+				return FALSE;
+		} while (InterlockedCompareExchange (&num_used_entries, index - 1, index) != index);
 
-	--index;
-
-	entry = get_entry (index);
-
-	while (InterlockedCompareExchange (&entry->state, STATE_BUSY, STATE_USED) != STATE_USED)
-		;
+		entry = get_entry (index - 1);
+	} while (InterlockedCompareExchange (&entry->state, STATE_BUSY, STATE_USED) != STATE_USED);
 
 	mono_memory_barrier ();
 
