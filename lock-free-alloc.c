45fb7e82b4530f16ae5f64c15f2271146f55c488
Mark Probst <mark.probst@gmail.com>
2012-11-27 10:20:50 +0100

    Fix use of `volatile`.

--- lock-free-alloc.c
+++ lock-free-alloc.c
@@ -37,12 +37,12 @@ typedef struct _MonoLockFreeAllocDescriptor Descriptor;
 struct _MonoLockFreeAllocDescriptor {
 	MonoLockFreeQueueNode node;
 	MonoLockFreeAllocator *heap;
-	Anchor anchor;
+    volatile Anchor anchor;
 	unsigned int slot_size;
 	unsigned int max_count;
 	gpointer sb;
 #ifndef DESC_AVAIL_DUMMY
-	Descriptor *next;
+	Descriptor * volatile next;
 #endif
 	gboolean in_use;
 };
